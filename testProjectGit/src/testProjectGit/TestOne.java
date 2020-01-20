01338907cdcb1919366b3a62ffdf4df2363d5c19
aakjain1 <aakjain1@WKWIN1302811.global.publicisgroupe.net>
2020-01-20 15:20:23 +0530

    Added new class TestOne

---
+++ testProjectGit/src/testProjectGit/TestOne.java
@@ -0,0 +1,10 @@
+package testProjectGit;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Git File");
+	}
+
+}
